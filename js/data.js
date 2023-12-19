7fb6292770453b6e07ec3d66cf2eb51d74a27b3c
Deep kar <107262501+Deepkaur004@users.noreply.github.com>
2023-12-20 00:49:52 +0530

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,74 +1,74 @@
 let songs = [
     {
         name : "Baller",
-        path : "../sounds/Baller.mp3",
+        path : "./sounds/Baller.mp3",
         artist : "Shubh",
-        cover : "../images/Shubh.jpg"
+        cover : "./images/Shubh.jpg"
     },
     {
         name : "Spaceship",
-        path : "../sounds/Spaceship.mp3",
+        path : "./sounds/Spaceship.mp3",
         artist : "Ap Dhillon",
-        cover : "../images/Ap-Dhillon.jpg"
+        cover : "./images/Ap-Dhillon.jpg"
     },
     {
         name : "Dhakka",
-        path : "../sounds/dhakka.mp3",
+        path : "./sounds/dhakka.mp3",
         artist : "Sidhu Moosewala",
-        cover : "../images/moosewala.jpg"
+        cover : "./images/moosewala.jpg"
     },
     {
         name : "Valaam",
-        path : "../sounds/valaam.mp3",
+        path : "./sounds/valaam.mp3",
         artist : "Arijit Singh",
-        cover : "../images/arijit-singh.png"
+        cover : "./images/arijit-singh.png"
     },
     {
         name : "Star Boy",
-        path : "../sounds/Starboy.mp3",
+        path : "./sounds/Starboy.mp3",
         artist : "The Weekend",
-        cover : "../images/weekend.jpg"
+        cover : "./images/weekend.jpg"
     },
     {
         name : "Perfect",
-        path : "../sounds/Perfect.mp3",
+        path : "./sounds/Perfect.mp3",
         artist : "ED Sheeran",
-        cover : "../images/ed-sheeran.jpg"
+        cover : "./images/ed-sheeran.jpg"
     },
     {
         name : "Rang jo Lagyo",
-        path : "../sounds/Rang-Jo-Lagyo.mp3",
+        path : "./sounds/Rang-Jo-Lagyo.mp3",
         artist : "Atif Aslam",
-        cover : "../images/rang-jo.jpg"
+        cover : "./images/rang-jo.jpg"
     },
     {
         name : "Tum Se Hi",
-        path : "../sounds/Tum-Se-Hi.mp3",
+        path : "./sounds/Tum-Se-Hi.mp3",
         artist : "Sonu Nigam",
-        cover : "../images/tum-se-hi.jpg"
+        cover : "./images/tum-se-hi.jpg"
     },
     {
         name : "Mileya Mileya",
-        path : "../sounds/mileya-mileya.mp3",
+        path : "./sounds/mileya-mileya.mp3",
         artist : "Rekha Bhardwaj",
-        cover : "../images/mileya.jpg"
+        cover : "./images/mileya.jpg"
     },
     {
         name : "Mast Magan",
-        path : "../sounds/Mast-Magan.mp3",
+        path : "./sounds/Mast-Magan.mp3",
         artist : "Arijit Singh",
-        cover : "../images/2-states.jpg"
+        cover : "./images/2-states.jpg"
     },
     {
         name : "Tuhi Mera",
-        path : "../sounds/Tuhi-Mera.mp3",
+        path : "./sounds/Tuhi-Mera.mp3",
         artist : "Pritam",
-        cover : "../images/jannat-2.jpg"
+        cover : "./images/jannat-2.jpg"
     },
     {
         name : "Ye Tune Kya Kiya",
-        path : "../sounds/Ye-Tune-Kya-Kiya.mp3",
+        path : "./sounds/Ye-Tune-Kya-Kiya.mp3",
         artist : "Pritam , Javed Bashir",
-        cover : "../images/yeh-tune.jpg"
+        cover : "./images/yeh-tune.jpg"
     }
 ]
